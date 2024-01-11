56981abe2bcf9465218d240c1efc6200c4cecdbf
Raedin Khaled Sakhri <ksakhri999@gmail.com>
2024-01-11 13:46:04 +0100

    HomePage and Navbar completed

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,48 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getTimeStamp = (createdAt: Date): string => {
+  const now = new Date();
+  const seconds = Math.floor((now.getTime() - createdAt.getTime()) / 1000);
+
+  let interval = seconds / 31536000; // Calculate years
+
+  if (interval > 1) {
+    return Math.floor(interval) + " years ago";
+  }
+  interval = seconds / 2592000; // Calculate months
+  if (interval > 1) {
+    return Math.floor(interval) + " months ago";
+  }
+  interval = seconds / 604800; // Calculate weeks
+  if (interval > 1) {
+    return Math.floor(interval) + " weeks ago";
+  }
+  interval = seconds / 86400; // Calculate days
+  if (interval > 1) {
+    return Math.floor(interval) + " days ago";
+  }
+  interval = seconds / 3600; // Calculate hours
+  if (interval > 1) {
+    return Math.floor(interval) + " hours ago";
+  }
+  interval = seconds / 60; // Calculate minutes
+  if (interval > 1) {
+    return Math.floor(interval) + " minutes ago";
+  }
+  return Math.floor(seconds) + " seconds ago";
+};
+
+export const formatNumber = (num: number): string => {
+  if (num >= 1000000) {
+    return (num / 1000000).toFixed(1) + "M";
+  } else if (num >= 1000) {
+    return (num / 1000).toFixed(1) + "K";
+  } else {
+    return num.toString();
+  }
+};
