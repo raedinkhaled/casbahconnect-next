78e5c610321602b9c21ac642dab44953bcb92c79
Raedin Khaled Sakhri <ksakhri999@gmail.com>
2024-01-12 13:59:35 +0100

    clerk webhook

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,6 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
-    "/api",
     "/api/webhook(.*)",
     "/question/:id",
     "/tags",
