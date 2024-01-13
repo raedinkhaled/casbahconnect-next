44b579dd343a13d069287f1ef1e6fb512830cfb8
Raedin Khaled Sakhri <ksakhri999@gmail.com>
2024-01-13 13:20:56 +0100

    tags page completed

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,17 +1,17 @@
 import QuestionForm from "@/components/forms/QuestionForm";
 import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
 
 import { redirect } from "next/navigation";
 import React from "react";
 
 const AskQuestion = async () => {
-  // const { userId } = auth();
-  const userId = "clerk12345";
+  const { userId } = auth();
+
   if (!userId) redirect("/sign-in");
 
   const mongoUser = await getUserById({ userId });
 
-  console.log(mongoUser);
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
